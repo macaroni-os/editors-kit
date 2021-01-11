4009d78daabfa6f36dcebaa8c9d8a9ec33c6f3fe
Daniel Robbins <drobbins@funtoo.org>
2021-01-11 11:52:28 -0700

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      fusion809: d20a8d27b85bb2735ab0d432c14916f46b50238c
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 2559b33e6e9cf7dd8464d5b8df555be614cbc722

--- app-editors/neovim/autogen.py
+++ app-editors/neovim/autogen.py
@@ -31,9 +31,9 @@ def generate_ebuild(hub, repo_name, release_data, is_stable, **pkginfo):
 
 async def generate(hub, **pkginfo):
 	name = pkginfo["name"]
-	release_data = await hub.pkgtools.fetch.get_page(
-		f"https://api.github.com/repos/{name}/{name}/releases", is_json=True
-	)
+	release_data = await hub.pkgtools.fetch.get_page(f"https://api.github.com/repos/{name}/{name}/releases", is_json=True)
 
 	generate_ebuild(hub, name, release_data, True, **pkginfo)
-	generate_ebuild(hub, name, release_data, False, **pkginfo)
+	# FL-7926 - previous (NVIM NVIM 0.5.095053284) unstable version has been
+	# removed from github
+	# generate_ebuild(hub, name, release_data, False, **pkginfo)
